c932c1ed524d59ab3ba5ae876374e1a7b6d35a56
danielsaraf <danielsaraf94@gmail.com>
2020-11-28 18:23:23 +0200

    test

--- DB_Project/Controllers/AccommodationController.cs
+++ DB_Project/Controllers/AccommodationController.cs
@@ -6,8 +6,6 @@ using DB_Project.Models;
 using DB_Project.Models.Contexts;
 using Microsoft.AspNetCore.Mvc;
 
-// For more  information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace DB_Project.Controllers
 {
     [Route("api/[controller]")]
