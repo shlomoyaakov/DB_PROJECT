721ff30211519ec4916942756921b6421309f80e
danielsaraf <danielsaraf94@gmail.com>
2020-12-26 17:51:54 +0200

    client update like map

--- DB_Project/wwwroot/js/plan_trip.js
+++ DB_Project/wwwroot/js/plan_trip.js
@@ -12,25 +12,31 @@ function showAccommodations() {
             jsonResponse = JSON.parse(this.response);
             if (jsonResponse.length == 0) {
                 document.getElementById("emptyAccommodations").classList.remove("d-none")
-                document.getElementById("accoNav").remove();
+                accommodationsList.remove();
             }
             else {
                 for (i in jsonResponse) {
-                    var li = document.createElement("li");
-                    li.appendChild(document.createTextNode(jsonResponse[i].name.replaceAll("\"", "")));
-                    accommodationsList.appendChild(li);
+                    let name = jsonResponse[i].name
+                    let internet = jsonResponse[i].internet
+                    let location = jsonResponse[i].location.coordinates
+                    let phone = jsonResponse[i].phone
+                    let type = jsonResponse[i].type
+
+                    var option = document.createElement("option");
+                    option.text = name
+                    option.value = "Internet: " + internet + "<br>Location: lat " + location.latitude + ", lng " + location.longitude + "<br>Phone: " + phone + "<br>Type: " + type
+                    accommodationsList.add(option, accommodationsList[0]);
                 }
             }
         }
         else {
             document.getElementById("emptyAccommodations").classList.remove("d-none")
-            document.getElementById("accoNav").remove();
+            accommodationsList.remove();
         }
         if (responses < 2) {
             responses++
         } else {
-            loadDiv.remove()
-            mainDiv.classList.remove("d-none")
+            pageLoaded()
         }
     };
     // ask the server for countries & cities list
@@ -47,37 +53,36 @@ function showRestaurants() {
             jsonResponse = JSON.parse(this.response);
             if (jsonResponse.length == 0) {
                 document.getElementById("emptyRestaurants").classList.remove("d-none")
-                document.getElementById("restNav").remove();
+                restaurantsList.remove();
             }
             else {
                 for (i in jsonResponse) {
-                    var li = document.createElement("li");
-                    li.appendChild(document.createTextNode(jsonResponse[i].name.replaceAll("\"", "")));
-                    restaurantsList.appendChild(li);
+                    let name = jsonResponse[i].name.replaceAll("\"", "")
+                    let cuisine = jsonResponse[i].cuisine.replaceAll(";",", ")
+                    let location = jsonResponse[i].location.coordinates
+                    let phone = jsonResponse[i].phone
+      
+                    var option = document.createElement("option");
+                    option.text = name
+                    option.value = "Cuisine: " + cuisine + "<br>Location: lat " + location.latitude + ", lng " + location.longitude  + "<br>Phone: " + phone 
+                    restaurantsList.add(option, restaurantsList[0]);
                 }
             }
         }
         else {
             document.getElementById("emptyRestaurants").textContent = 'Error occure while getting restaurants from server'
-            document.getElementById("restNav").remove();
+            restaurantsList.remove();
         }
         if (responses < 2) {
             responses++
         } else {
-            loadDiv.remove()
-            mainDiv.classList.remove("d-none")
+            pageLoaded()
         }
     };
     // ask the server for countries & cities list
-    // bug that need to be fixed!
-     // xhttp.open("GET", "/api/Restaurants/location?country=" + country + "&city=" + city);
-    //xhttp.send();
-      if (responses < 2) {
-            responses++
-        } else {
-            loadDiv.remove()
-            mainDiv.classList.remove("d-none")
-        }
+      xhttp.open("GET", "/api/Restaurants/location?country=" + country + "&city=" + city);
+    xhttp.send();
+
 }
 
 function showAttractions() {
@@ -88,25 +93,30 @@ function showAttractions() {
             jsonResponse = JSON.parse(this.response);
             if (jsonResponse.length == 0) {
                 document.getElementById("emptyAttractions").classList.remove("d-none")
-                document.getElementById("AttracNav").remove();
+                attractionsList.remove();
             }
             else {
                 for (i in jsonResponse) {
-                    var li = document.createElement("li");
-                    li.appendChild(document.createTextNode(jsonResponse[i].name.replaceAll("\"", "")));
-                    attractionsList.appendChild(li);
+                    let name = jsonResponse[i].name.replaceAll("\"", "")
+                    let location = jsonResponse[i].location.coordinates
+                    let phone = jsonResponse[i].phone
+
+
+                    var option = document.createElement("option");
+                    option.text = name
+                    option.value = "Location: lat " + location.latitude + ", lng " + location.longitude  + "<br>Phone: " + phone
+                    attractionsList.add(option, attractionsList[0]);
                 }
             }
         }
         else {
             document.getElementById("emptyAttractions").textContent = 'Error occure while getting attractions from server';
-            document.getElementById("AttracNav").remove();
+            attractionsList.remove();
         }
         if (responses < 2) {
             responses++
         } else {
-            loadDiv.remove()
-            mainDiv.classList.remove("d-none")
+            pageLoaded()
         }
     };
     // ask the server for countries & cities list
@@ -125,10 +135,71 @@ document.getElementById("travelId").textContent += city + ", " + country;
 var accommodationsList = document.getElementById("accommodationsList");
 var restaurantsList = document.getElementById("restaurantsList");
 var attractionsList = document.getElementById("attractionsList");
-
+var map
+var marker = ""
 
 window.onload = function () {
     showAccommodations();
     showRestaurants();
     showAttractions();
+    initMap()
+}
+
+let pageLoaded = function () {
+    loadDiv.remove()
+    mainDiv.classList.remove("d-none")
+    $(".selection-2").select2({
+        minimumResultsForSearch: 20,
+        dropdownParent: $('#dropDownSelect1')
+    });
+
+    $('option').mousedown(function (e) {
+        e.preventDefault();
+        $(this).prop('selected', !$(this).prop('selected'));
+        var location = getConstLocation($(this).prop('value'))
+        setMarker(location)
+        return false;
+    });
+
+    $('option').mouseover(function (e) {
+        e.preventDefault();
+        $('#hoverlabel').text($(this).prop('label'))
+
+        document.getElementById("hovervalue").innerHTML = $(this).prop('value')
+        return false;
+    });
+
+    $('select').mouseleave(function (e) {
+        e.preventDefault();
+        $('#hoverlabel').text("")
+        $('#hovervalue').text("")
+        return false;
+    });}
+function initMap() {
+    let options = {
+        zoom: 3,
+        center: { lat: 32.3232919, lng: 34.85538661 },
+    }
+    map = new google.maps.Map(document.getElementById("map"), options);
+
+}
+function getConstLocation(value) {
+   //"Internet: N/A<br>Location: 48.8824615, 2.3498469<br>Phone: N/A<br>Type: hotel"
+    var latitude = value.match("lat (.*),")[1];
+    var longitude = value.match("lng (.*)<br>P")[1];
+
+    const myLatLng = { lat: parseFloat(latitude), lng: parseFloat(longitude) };
+    return myLatLng
+}
+function setMarker(location) {
+    if (marker !== "") {
+        marker.setMap(null)
+    }
+    marker = new google.maps.Marker({
+        position: location,
+        map,
+        title: "Hello World!",
+    });
+    map.setCenter(location)
+    map.setZoom(13)
 }
