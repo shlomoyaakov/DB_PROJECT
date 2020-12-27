4758a12c51385436339ba58a11f2d39eab9ab737
danielsaraf <danielsaraf94@gmail.com>
2020-12-27 08:24:54 +0200

    client updates

--- DB_Project/wwwroot/js/plan_trip.js
+++ DB_Project/wwwroot/js/plan_trip.js
@@ -152,36 +152,28 @@ let pageLoaded = function () {
         minimumResultsForSearch: 20,
         dropdownParent: $('#dropDownSelect1')
     });
+       
 
     $('option').mousedown(function (e) {
         e.preventDefault();
         $(this).prop('selected', !$(this).prop('selected'));
         var location = getConstLocation($(this).prop('value'))
         setMarker(location)
-        return false;
-    });
-
-    $('option').mouseover(function (e) {
-        e.preventDefault();
         $('#hoverlabel').text($(this).prop('label'))
-
         document.getElementById("hovervalue").innerHTML = $(this).prop('value')
         return false;
     });
 
-    $('select').mouseleave(function (e) {
-        e.preventDefault();
-        $('#hoverlabel').text("")
-        $('#hovervalue').text("")
-        return false;
-    });}
+
+}
+
+
 function initMap() {
     let options = {
         zoom: 3,
         center: { lat: 32.3232919, lng: 34.85538661 },
     }
     map = new google.maps.Map(document.getElementById("map"), options);
-
 }
 function getConstLocation(value) {
    //"Internet: N/A<br>Location: 48.8824615, 2.3498469<br>Phone: N/A<br>Type: hotel"
@@ -202,4 +194,63 @@ function setMarker(location) {
     });
     map.setCenter(location)
     map.setZoom(13)
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("saveBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    var details = document.getElementById("tripDetails")
+    var acclist= ""
+    var reslist = ""
+    var attlist = ""
+
+    for (var i = 0, len = accommodationsList.options.length; i < len; i++) {
+        opt = accommodationsList.options[i];
+        if (opt.selected) {
+            acclist += opt.text+", "
+        }
+    }
+    acclist = acclist.substring(0, acclist.length-2)
+
+    for (var i = 0, len = restaurantsList.options.length; i < len; i++) {
+        opt = restaurantsList.options[i];
+        if (opt.selected) {
+            reslist += opt.text + ", "
+        }
+    }
+    reslist = reslist.substring(0, reslist.length - 2)
+
+    for (var i = 0, len = attractionsList.options.length; i < len; i++) {
+        opt = attractionsList.options[i];
+        if (opt.selected) {
+            attlist += opt.text + ", "
+        }
+    }
+    attlist = attlist.substring(0, attlist.length - 2)
+
+
+    details.innerHTML = "<br><strong>Accommodations: </strong >" + acclist + "<br><strong>Restaurants: </strong>" + reslist + "<br><strong>Attractions: </strong>" + attlist
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    closeModal()
+}
+var closeModal = function () {
+    modal.style.display = "none";}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
