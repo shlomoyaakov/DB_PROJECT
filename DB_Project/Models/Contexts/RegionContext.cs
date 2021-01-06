89c655aa4664869c55b64dea4521d116d53e5ada
shlomoyaakov <shlomo4945@gmail.com>
2021-01-06 14:47:34 +0200

    Added complex query and removed unnecessary function in region controller and context

--- DB_Project/Models/Contexts/RegionContext.cs
+++ DB_Project/Models/Contexts/RegionContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using DB_Project.Models.Contexts;
+using DB_Project.Models.Data_Class;
 using MySql.Data.MySqlClient;
 
 namespace DB_Project.Models
@@ -8,9 +9,7 @@ namespace DB_Project.Models
     public class RegionContext : BaseContext
     {
 
-        public RegionContext(string connectionString) : base(connectionString)
-        {
-        }
+        public RegionContext(string connectionString) : base(connectionString) {}
 
   
 
@@ -81,25 +80,53 @@ namespace DB_Project.Models
             }
         }
 
-        public List<KeyValuePair<string, Int64>> Get_Amount_By_Country()
+        private void Get_Data_Per_Region_By_Req(string request, Dictionary<string, Stats> stats_map,
+            MySqlConnection conn, string type)
         {
-            List<KeyValuePair<string, Int64>> list = new List<KeyValuePair<string, Int64>>();
-            string req = "select country, count(country) as amount from trip_region " +
-                        "group by country " +
-                        $"ORDER  by amount DESC;";
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(request, conn);
+                using var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string city = reader["city"].ToString();
+                    if (!stats_map.ContainsKey(city))
+                    {
+                        Region reg = new Region()
+                        {
+                            City = city
+                        };
+                        stats_map[city] = new Stats()
+                        {
+                            General_Location = reg
+                        };
+                    }
+                    stats_map[city].Data[type] = (Int64)reader["amount"];
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+        private List<Stats> Get_Activities_By_Req(string att_req, string rest_req, string acc_req, string trips_req)
+        {
+            List<Stats> list = new List<Stats>();
+            Dictionary<string, Stats> stats_map = new Dictionary<string, Stats>();
             try
             {
                 using (MySqlConnection conn = GetConnection())
                 {
                     conn.Open();
-                    MySqlCommand cmd = new MySqlCommand(req, conn);
-                    using var reader = cmd.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        KeyValuePair<string, Int64> kv = new KeyValuePair<string, Int64>(reader["country"].ToString()
-                            , (Int64)reader["amount"]);
-                        list.Add(kv);
-                    }
+                    Get_Data_Per_Region_By_Req(rest_req, stats_map, conn,"Restaurants");
+                    Get_Data_Per_Region_By_Req(att_req, stats_map, conn, "Attractions");
+                    Get_Data_Per_Region_By_Req(acc_req, stats_map, conn, "Accommodation");
+                    Get_Data_Per_Region_By_Req(trips_req, stats_map, conn, "Trips");
+                }
+                foreach (KeyValuePair<string, Stats> entry in stats_map)
+                {
+                    list.Add(entry.Value);
                 }
             }
             catch (Exception e)
@@ -109,33 +136,45 @@ namespace DB_Project.Models
             return list;
         }
 
-        public List<KeyValuePair<string, int>> Get_Amount_By_City(string country)
+        public List<Stats> Get_Stats_Per_Region(string country)
         {
-            List<KeyValuePair<string, int>> list = new List<KeyValuePair<string, int>>();
-            string req = "select city, count(city) as amount from trip_region " +
+            List<Stats> ret_list;
+            string att_req = "select city, count(city) as amount from attractions join places on attractions.lat = " +
+                        "places.lat and attractions.lon = places.lon " +
+                        $"where country = \"{country}\" " +
+                        "group by city " +
+                        $"ORDER  by amount DESC;";
+
+            string rest_req = "select city, count(city) as amount from restaurants join places on restaurants.lat = " +
+                         "places.lat and restaurants.lon = places.lon " +
+                         $"where country = \"{country}\" " +
+                         "group by city " +
+                         $"ORDER  by amount DESC;";
+
+            string acc_req = "select city, count(city) as amount from accommodation join places on accommodation.lat = " +
+                         "places.lat and accommodation.lon = places.lon " +
+                         $"where country = \"{country}\" " +
+                         "group by city " +
+                         $"ORDER  by amount DESC;";
+
+            string trips_req = "select city, count(city) as amount from trip_region " +
                         $"where country = \"{country}\"" +
                         "group by city " +
                         $"ORDER  by amount DESC;";
+
             try
             {
-                using (MySqlConnection conn = GetConnection())
+                ret_list = Get_Activities_By_Req(att_req, rest_req, acc_req, trips_req);
+                foreach(Stats s in ret_list)
                 {
-                    conn.Open();
-                    MySqlCommand cmd = new MySqlCommand(req, conn);
-                    using var reader = cmd.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        KeyValuePair<string, int> kv = new KeyValuePair<string, int>(reader["city"].ToString()
-                            , (int)reader["amount"]);
-                        list.Add(kv);
-                    }
+                    s.General_Location.Country = country;
                 }
+                return ret_list;
             }
             catch (Exception e)
             {
                 throw e;
             }
-            return list;
         }
     }
 }
